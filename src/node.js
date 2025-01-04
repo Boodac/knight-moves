4a1622c6444028bb0ccdbe23e181f77a3cc48e36
boodac <180095661+Boodac@users.noreply.github.com>
2025-01-04 14:38:51 -0500

    add a findEdges method to the node class, finalizing it for now

--- src/node.js
+++ src/node.js
@@ -1,7 +1,9 @@
 export default class Node {
     #coordinate // the internal coordinate represented as [intX, intY]
+    #cipher = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8 };
     constructor(coordinate) { 
         this.#coordinate = this.#splitCoords(coordinate);
+        this.edges = this.findEdges();
     };
 
     get coordinate() { // the external coordinate, in chess notation
@@ -9,45 +11,39 @@ export default class Node {
     };
 
     findEdges(node = this) {
-        let xCoord = node.#coordinate[0];
-        let yCoord = node.#coordinate[1];
+        const xCoord = node.#coordinate[0];
+        const yCoord = node.#coordinate[1];
+        const flower = {
+            xmTwo_ymOne: [xCoord-2, yCoord-1],
+            xmTwo_ypOne: [xCoord-2, yCoord+1],
+            xpTwo_ymOne: [xCoord+2, yCoord-1],
+            xpTwo_ypOne: [xCoord+2, yCoord+1],
 
-        /*
+            xmOne_ymTwo: [xCoord-1, yCoord-2],
+            xpOne_ymTwo: [xCoord+1, yCoord-2],
+            xmOne_ypTwo: [xCoord-1, yCoord+2],
+            xpOne_ypTwo: [xCoord+1, yCoord+2],
+        };
 
-        assume e4 = 5, 4;
-        edges:
-            g3 = 7, 3
-            g5 = 7, 5
-            f6 = 6, 6
-            f2 = 6, 2
-            d6 = 4, 6
-            d2 = 4, 2
-            c3 = 3, 3
-            c5 = 5, 5
-
-        therefore we have sets of: 
-
-        xCoords: [3 - 7]
-        yCoords: [2 - 6]
-        so two sets of X-2, X-1, X+2, X+1
-        and Y-2, Y-1, Y+2, Y+1
-
-        if x or y < 0 or > 8, return false;
-
-        */
-    }
+        return Object.values(flower).filter(coordinate => {
+            if(coordinate[0] <= 8 && coordinate[0] > 0 &&
+               coordinate[1] <= 8 && coordinate[1] > 0) {
+                return this.#joinCoords(coordinate);
+            }
+        }); // this filters the legal moves into an array
+    };
 
     #splitCoords(coordinate) {
         let firstCoord = coordinate[0];
-        if(cipher[firstCoord]) firstCoord = cipher[firstCoord];
-        let secondCoord = parseInt(coordinate[1]);
+        if(this.#cipher[firstCoord]) firstCoord = this.#cipher[firstCoord];
+        const secondCoord = coordinate[1];
         return [parseInt(firstCoord), secondCoord];
     };
 
     #joinCoords(array) {
         let string = "";
-        for(const key in cipher) {
-            if(cipher[key] === array[0]) {
+        for(const key in this.#cipher) {
+            if(this.#cipher[key] === array[0]) {
                 string += key;
             }
         }
@@ -56,11 +52,6 @@ export default class Node {
 
 };
 
-const cipher = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8 };
+const node = new Node([7,3]);
 
-
-const node = new Node([1,2]);
-const node2 = new Node("b4");
-
-console.log(node.coordinate);
-console.log(node2.coordinate);
+console.log(node.edges);
