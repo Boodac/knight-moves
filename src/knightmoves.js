0d6f63da55f3bf5606a48c9e5c0a48b2ef0948b2
boodac <180095661+Boodac@users.noreply.github.com>
2025-01-05 00:08:44 -0500

    finally get this thing to work

--- src/knightmoves.js
+++ src/knightmoves.js
@@ -1,10 +1,12 @@
-import Node from "./node.js";
 import Graph from "./graph.js";
+import Node from "./node.js";
 
 function knightMoves(initialPosition, endPosition) {
-    if(!endPosition) throw new Error("missing arguments at knightMoves()");
-    const start = new Node(initialPosition);
-    const end = new Node(endPosition);
+    if(!initialPosition || !endPosition) throw new Error("missing arguments at knightMoves()");
+    const graph = new Graph();
+    const start = graph.find(initialPosition);
+    const end = graph.find(endPosition);
+    const result = [];
     if(start.coordinate == end.coordinate) {
         console.log("It takes a minimum of 4 moves for a knight to return to its starting position.");
         console.log("This is a bit silly, but, if you insist, here is an example:");
@@ -17,8 +19,38 @@ function knightMoves(initialPosition, endPosition) {
         console.log(`End on move 4: ${end.coordinate}`);
         return "... happy?";
     };
-    const queue = [start];
-    const result = [start.coordinate];
-    const visited = new Set();
 
+    const traverse = (node, end) => {
+        const result = [];
+        const queue = [];
+        queue.push([node, result]);
+        const visited = new Set();
+
+        while(queue.length) {
+            const currentOp = queue.shift();
+            let currentNode = currentOp[0];
+            if(!(currentNode instanceof Node)) currentNode = graph.find(currentNode);
+            const priorPath = currentOp[1];
+
+            if(visited.has(currentNode)) continue;
+            else visited.add(currentNode);
+
+            if(currentNode === end) return priorPath.concat(end.coordinate);
+            
+            else {
+                for(const coord of currentNode.edges) {
+                    let node = graph.find(coord);
+                    if(visited.has(node)) continue;
+                    queue.push([node, priorPath.concat(currentNode.coordinate)]);
+                }
+            };
+            
+        };
+    };
+
+    return traverse(start, end);
 };
+
+const result = knightMoves([3,3],[4,3]);
+console.log(`From ${result[0]} to ${result[result.length-1]} in ${result.length-1} moves`);
+result.forEach(move => console.log(move));
